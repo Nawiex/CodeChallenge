af8e77ee58faf34bb700b5de49f8af57fcf83b86
Carlos Aranibar <nawiex@outlook.com>
2022-03-22 01:51:36 -0500

    Add files via upload

---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Descomentar para Depurar:
+            args = new string[2];
+            args[0] = "javascript";
+            args[1] = "python";
+
+
+            Console.WriteLine("Code Challenge : SearchFight - nawiex");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No se especificaron terminos de busqueda. Intentar de nuevo.");
+                return;
+            }
+            Init(args).GetAwaiter().GetResult();
+        }
+
+        static async Task Init(string[] args)
+        {
+            await Searchfight.CompararBuscadores(args.ToList());
+            //SearchFightKernel.Reports.ForEach(report => Console.WriteLine(report));
+        }
+    }
+}
---
+++ CodeChallenge/Searchfight.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeChallenge.Interfaces;
+using CodeChallenge.Implementaciones;
+using CodeChallenge.Modelos;
+
+namespace CodeChallenge
+{
+    public static class Searchfight
+    {
+        #region Atributos
+
+        public static List<string> ListaReportes { get; private set; }
+
+        #endregion
+
+        #region Servicios
+
+        static IServicioBusqueda ServicioBusqueda;
+        static IServicioGanadores ServicioGanadores;
+        static IServicioReportes ServicioReportes;
+
+        #endregion
+
+        #region Constructores
+
+        static Searchfight()
+        {
+            // Initializar dependencias
+            ServicioBusqueda = new ServicioBusqueda();
+            ServicioGanadores = new ServicioGanadores();
+            ServicioReportes = new ServicioReportes();
+
+            // Initialize resultados
+            ListaReportes = new List<string>();
+        }
+
+        #endregion
+
+        #region Metodos Publicos
+
+        public static async Task CompararBuscadores(IList<string> terminos)
+        {
+            IList<Busqueda> searchData = await ServicioBusqueda.ObtenerResultadosBusqueda(terminos);
+            IEnumerable<Ganador> searchEngineWinners = ServicioGanadores.ObtenerGanadores(searchData);
+            Ganador grandWinner = ServicioGanadores.ObtenerGanador(searchData);
+
+            ListaReportes.AddRange(ServicioReportes.ObtenerReporteBusqueda(searchData));
+            ListaReportes.AddRange(ServicioReportes.ObtenerReporteGanadores(searchEngineWinners));
+            ListaReportes.Add(ServicioReportes.ObtenerGanador(grandWinner));
+
+
+            foreach (string item in ListaReportes) {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+
+        }
+
+        #endregion
+    }
+}
+
